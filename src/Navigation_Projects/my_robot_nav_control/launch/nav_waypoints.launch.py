75e473206649ab361f013b7c77b959dbdb5d3956
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2026-03-23 14:40:57 +0100

    nav launch

--- src/Navigation_Projects/my_robot_nav_control/launch/nav_waypoints.launch.py
+++ src/Navigation_Projects/my_robot_nav_control/launch/nav_waypoints.launch.py
@@ -3,21 +3,24 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('my_robot_nav_control')
 
-    default_wp_file = os.path.join(pkg_share, 'config', 'waypoints_sw.yaml')
-
-    wp_file_arg = DeclareLaunchArgument(
-        'wp_file',
-        default_value=default_wp_file,
-        description='Path to external waypoint YAML file'
+    wp_name_arg = DeclareLaunchArgument(
+        'wp_name',
+        default_value='waypoints_sw.yaml',
+        description='Waypoint YAML filename inside package config folder'
     )
+
+    wp_file = PathJoinSubstitution([
+        FindPackageShare('my_robot_nav_control'),
+        'config',
+        LaunchConfiguration('wp_name')
+    ])
 
     nav_waypoints_node = Node(
         package='my_robot_nav_control',
@@ -25,11 +28,11 @@ def generate_launch_description():
         name='nav_waypoints_node',
         output='screen',
         parameters=[
-            {'wp_file': LaunchConfiguration('wp_file')}
+            {'wp_file': wp_file}
         ]
     )
 
     return LaunchDescription([
-        wp_file_arg,
+        wp_name_arg,
         nav_waypoints_node
     ])
